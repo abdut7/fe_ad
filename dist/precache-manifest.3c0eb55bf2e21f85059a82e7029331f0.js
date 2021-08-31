0660a877f2021d3d351140dc1ede282e8047f307
sethumadhavan-k <sethuksethu@gmail.com>
2021-08-31 07:47:20 +0530

    user side

---
+++ dist/precache-manifest.3c0eb55bf2e21f85059a82e7029331f0.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b193e4d4dc7c5ec9a3c88626b2c736",
+    "url": "/coreui-utils.js"
+  },
+  {
+    "revision": "7552ca9b77e12d603f0475124145ff07",
+    "url": "/coreui.bundle.min.js"
+  },
+  {
+    "revision": "d4811099b8e1c63ff00a",
+    "url": "/css/app.7c66de84.css"
+  },
+  {
+    "revision": "f7f67cf595dcd0bc65e7",
+    "url": "/css/chunk-vendors.fe7371ac.css"
+  },
+  {
+    "revision": "0c406a8dd7c8a809edd6a87c178b5694",
+    "url": "/img/8.0c406a8d.jpg"
+  },
+  {
+    "revision": "3ce285d861b0176ec7ded6437e2fb483",
+    "url": "/img/bell-regular 1.3ce285d8.svg"
+  },
+  {
+    "revision": "365d60f78e036ff8894cf85244aa8689",
+    "url": "/img/bg.365d60f7.jpg"
+  },
+  {
+    "revision": "4077cd0a5512f9c207e9c21137c86726",
+    "url": "/img/calendar-week-solid.4077cd0a.svg"
+  },
+  {
+    "revision": "e9a34b3539bd9e151187337c1b2cd54f",
+    "url": "/img/check-circle-regular.e9a34b35.svg"
+  },
+  {
+    "revision": "856e7c7a8f15b59902bc60cb40b37683",
+    "url": "/img/check-circle-solid.856e7c7a.svg"
+  },
+  {
+    "revision": "079fbb56d1c8a44582c8457ccabde8d4",
+    "url": "/img/clock-regular.079fbb56.svg"
+  },
+  {
+    "revision": "ac181acdc9147bf507440e996149233c",
+    "url": "/img/cog-solid.ac181acd.svg"
+  },
+  {
+    "revision": "d9602419915277e67c28fdea542357b3",
+    "url": "/img/cog-solid.d9602419.svg"
+  },
+  {
+    "revision": "dc10e52417069ed0661f0d195c90057a",
+    "url": "/img/coreui.dc10e524.svg"
+  },
+  {
+    "revision": "e4e45abfb5ccfd68547c3f5bd9597d9c",
+    "url": "/img/credit-card-solid.e4e45abf.svg"
+  },
+  {
+    "revision": "6e997da50af314f954da8ab735e464ea",
+    "url": "/img/dollar-sign-solid.6e997da5.svg"
+  },
+  {
+    "revision": "42f851fa55a9f0968a88e6741c259760",
+    "url": "/img/envelope-open-text-solid.42f851fa.svg"
+  },
+  {
+    "revision": "c26381c168286db873a23d574a0596ff",
+    "url": "/img/expand_more_black_24dp.c26381c1.svg"
+  },
+  {
+    "revision": "fed875f2ada1a4e18048d0fa911e26ff",
+    "url": "/img/free.fed875f2.svg"
+  },
+  {
+    "revision": "4659ab6fac9721689c0b6aa38561842a",
+    "url": "/img/graph.4659ab6f.svg"
+  },
+  {
+    "revision": "19627cff49c31c774e03f0b8447be0da",
+    "url": "/img/histore-refresh.19627cff.svg"
+  },
+  {
+    "revision": "91e15d513ccc201aef7803ef9b41a0f8",
+    "url": "/img/history-dots.91e15d51.svg"
+  },
+  {
+    "revision": "1422b27cf17264487ca83636f5fa74a4",
+    "url": "/img/increacse2.1422b27c.svg"
+  },
+  {
+    "revision": "442e93cef443f488e96d75dd2f0db0f6",
+    "url": "/img/lock-solid.442e93ce.svg"
+  },
+  {
+    "revision": "c816aeeafdcb08da97094d10519a8e1b",
+    "url": "/img/newspaper-solid.c816aeea.svg"
+  },
+  {
+    "revision": "293802900e93d32b2d948a0906c602cb",
+    "url": "/img/play-active.29380290.svg"
+  },
+  {
+    "revision": "1f43c4dca8769d1816718235e8a2dce2",
+    "url": "/img/search-solid.1f43c4dc.svg"
+  },
+  {
+    "revision": "ec79165657ebfa52fca70439a0a67f40",
+    "url": "/img/shapes-solid.ec791656.svg"
+  },
+  {
+    "revision": "0ec2729fb6a6e6533eaf0a712ac5407e",
+    "url": "/img/star-solid.0ec2729f.svg"
+  },
+  {
+    "revision": "80ddfa8c9960ca744e20e6d41804489e",
+    "url": "/img/stream-solid.80ddfa8c.svg"
+  },
+  {
+    "revision": "753354f1c848fe77f1823d0ee5191a41",
+    "url": "/img/university-solid.753354f1.svg"
+  },
+  {
+    "revision": "9bb300ae79ac8f6ead22f881d8b2d04a",
+    "url": "/img/unlock.9bb300ae.svg"
+  },
+  {
+    "revision": "80f1854e7117ced9c908abb52334695b",
+    "url": "/img/user-alt-solid.80f1854e.svg"
+  },
+  {
+    "revision": "7ec7b151a588cf0e4d54cf06b29efbd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4811099b8e1c63ff00a",
+    "url": "/js/app.2f3839de.js"
+  },
+  {
+    "revision": "f7f67cf595dcd0bc65e7",
+    "url": "/js/chunk-vendors.2ab7dcdb.js"
+  },
+  {
+    "revision": "8210cb16373a4bfb0f83d1c438f06bb1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
